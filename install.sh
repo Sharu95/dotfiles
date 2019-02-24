7aae0e2b03d0584732b1d5ae90db0556ac1740bc
Sharu95 <sharanak@uio.no>
2019-02-24 10:36:44 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+BASH_PROFILE=".bash_profile"
+CONFIG_FOLDER="test"
+mkdir $HOME/$CONFIG_FOLDER
+
+touch $HOME/.bash_aliases
+mv $HOME/.bash_aliases $HOME/$BASH_PROFILE
+echo 'export CONFIG_FOLDER=$HOME/Configurations' >> $HOME/$BASH_PROFILE
+echo 'alias restart=source $HOME/.bashrc' >> $HOME/$BASH_PROFILE
+cd $HOME/$CONFIG_FOLDER
+
+# Config files
+git clone git@github.com:Sharu95/config.git config
+cat config/scripts/start_env.sh >> $HOME/$BASH_PROFILE
+cp config/.vimrc $HOME/.vimrc
+
+# Git-radar
+git clone git@github.com:michaeldfallen/git-radar.git git-radar
+echo 'export PATH=$PATH:$CONFIG_FOLDER/git-radar'
+echo 'LAMBDA="\[\033\]\[[38;5;208m\]λ\[\033\]\[[0m\]"' >> $HOME/$BASH_PROFILE
+echo 'export PS1="$LAMBDA\$(git-radar --bash --fetch) "'
+
+# Z jumping
+git clone git@github.com:rupa/z.git z
+echo '. $CONFIG_FOLDER/z/z.sh' >> $HOME/BASH_PROFILE
+
+# Ubuntu theme and tweak tools
+sudo apt update
+sudo apt install gnome-tweak-tool
+sudo apt install oranchelo-icon-theme
